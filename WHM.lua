b0ea658688f6fa784e269dcae2984e0757db7ca3
Tew187 <60381764+Tew187@users.noreply.github.com>
2020-05-16 17:24:20 -0500

    Updated gear and coding

--- WHM.lua
+++ WHM.lua
@@ -1,173 +1,194 @@
 function get_sets()			
+	-- Tables
+	BarElement = T{'Barfire', 'Barfira', 'Barblizzard', 'Barblizzara', 'Baraero', 'Baraera', 'Barstone', 'Barstonra', 'Barthunder', 'Barthundra', 'Barwater', 'Barwatera'}
 
 	sets.MoveSpeed = { feet = "Herald's Gaiters",}    --auto swaps when moving
-    send_command('input //equipviewer pos 1663 934')
 	
-	--TP Sets--
-	sets.WHM = {}
 	
-	sets.WHM.Idle = {
-		main="Queller Rod", -- 1
-		sub="Genbu's Shield",
-		ammo="Homiliary", -- 1
-		head="Inyanga Tiara +2", -- 2
-		neck="Cleric's torque",
-		ear2="Moonshade earring", -- 1
-		ear1="Novia earring",
-		body="Piety briault +3", -- 3
-		hands="Inyanga Dastanas +2",
-		ring1="Stikini Ring +1", -- 1
-		ring2="Stikini Ring +1", -- 1
-		back="Alaunus's Cape",
-		waist="Fucho-no-obi", -- ~1
-		legs="Inyanga Shalwar +2",
-		feet="Chironic Slippers" -- 2
+	sets.Idle = {
+		main		="Queller Rod",
+		sub			="Genbu's Shield",
+		ammo		="Homiliary",
+		head		="Inyanga Tiara +2",
+		neck		="Cleric's torque",
+		right_ear	="Moonshade earring",
+		left_ear	="Novia earring",
+		body		="Piety briault +3",
+		hands		="Inyanga Dastanas +2",
+		left_ring	="Stikini Ring +1",
+		right_ring	="Stikini Ring +1",
+		back		="Alaunus's Cape",
+		waist		="Fucho-no-obi",
+		legs		="Inyanga Shalwar +2",
+		feet		="Chironic Slippers"
 	}
 				
-	sets.WHM.FastCast = {
-		main="Grioavolr",
-		neck="Cleric's Torque",
-		head="Nahtirah Hat",
-		body="Inyanga Jubbah +2",
-		ammo="Incantor Stone",
-		hands="Chironic gloves",
-		ear1="Loquac. Earring",
-		ear2="Malignance earring",
-		feet="Regal Pumps +1",
-		legs="Ayanmo cosciales +2",
-		ring1="Prolix Ring",
-		ring2="Kishar Ring",
-		waist="Embla Sash"
+	sets.precast.FastCast = {
+		main		="Grioavolr",
+		neck		="Cleric's Torque",
+		head		="Nahtirah Hat",
+		body		="Inyanga Jubbah +2",
+		ammo		="Incantor Stone",
+		hands		="Chironic gloves",
+		left_ear	="Loquac. Earring",
+		right_ear	="Malignance earring",
+		feet		="Regal Pumps +1",
+		legs		="Ayanmo cosciales +2",
+		left_ring	="Prolix Ring",
+		right_ring	="Kishar Ring",
+		waist		="Embla Sash"
 	}
 
-	sets.WHM.Melee = {
-		main="Yagrush",
-		sub="Genbu's Shield"
+	sets.Engaged = {
+		main		="Yagrush",
+		sub			="Genbu's Shield"
 	}	
 	
-	sets.WHM.Resting = {
-		main="Queller Rod",
-		sub="Genbu's Shield",
-		ammo="Homiliary",
-		head="Nefer Khat",
-		neck="Eidolon Pendant",
-		ear1="Magnetic earring",
-		ear2="Moonshade earring",
-		body="Orvail Robe",
-		hands="Oracle's Gloves",
-		ring1="Janniston Ring",
-		ring2="Tamas Ring",
-		back="Felicitas Cape",
-		waist="Austerity Belt",
-		legs="Nisse Slacks",
-		feet="Avocat Pigaches"
+	sets.Resting = {
+		main		="Queller Rod",
+		sub			="Genbu's Shield",
+		ammo		="Homiliary",
+		head		="Nefer Khat",
+		neck		="Eidolon Pendant",
+		left_ear	="Magnetic earring",
+		right_ear	="Moonshade earring",
+		body		="Orvail Robe",
+		hands		="Oracle's Gloves",
+		left_ring	="Janniston Ring",
+		right_ring	="Tamas Ring",
+		back		="Felicitas Cape",
+		waist		="Austerity Belt",
+		legs		="Nisse Slacks",
+		feet		="Avocat Pigaches"
 	}
 	
-	sets.WHM.Healing = set_combine(sets.WHM.Idle,{
-		main="Yagrush",
-		sub="Genbu's Shield",
-		head="Orison Cap +2",
-		neck="Cleric's torque",
-		ear1="Magnetic Earring",
-		ear2="Loquacious Earring",
-		body="Orvail Robe",
-		hands="Orison Mitts +2",
-		body="Ebers bliaud +1",
-		feet="Eber's Duckbills +1"
+	sets.midcast.Healing = set_combine(sets.Idle,{
+		main		="Yagrush",
+		sub			="Genbu's Shield",
+		head		="Orison Cap +2",
+		neck		="Cleric's torque",
+		left_ear	="Magnetic Earring",
+		right_ear	="Loquacious Earring",
+		body		="Orvail Robe",
+		hands		="Orison Mitts +2",
+		body		="Ebers bliaud +1",
+		feet		="Ebers Duckbills +1"
 	})
 
-	sets.WHM.Cursna = set_combine(sets.WHM.Healing,{
-		main="Yagrush",
-		ring1="Haoma's Ring",
-		ring2="Menelaus's's Ring",
-		body="Ebers bliaud +1",		
-		neck="Debilis Medallion",
-		Hands="Fanatic Gloves",
-		legs="Theophany Pantaloons +3",
-		back="Alaunus's Cape",
-		feet="Vanya Clogs"
+	sets.midcast.Cursna = set_combine(sets.Healing,{
+		left_ring	="Haoma's Ring",
+		right_ring	="Menelaus's's Ring",
+		body		="Ebers bliaud +1",		
+		neck		="Debilis Medallion",
+		right_ear	="Meili Earring",
+		Hands		="Fanatic Gloves",
+		legs		="Theophany Pantaloons +3",
+		back		="Alaunus's Cape",
+		feet		="Vanya Clogs"
 	})	
 	
-	sets.WHM.Cure = set_combine(sets.WHM.Idle,{
-		main="Queller Rod",
-		ammo="Hydrocera",
-		head="Ebers cap +1",
-		neck="Cleric's torque",
-		body="Ebers bliaud +1",
-		ear1="Nourishing Earring +1",
-		ear2="Glorious Earring",
-		hands="Bokwus Gloves",
-		back="Alaunus's Cape",
-		waist="Witful Belt",
-		ring1="Janniston Ring",
-		ring2="Tamas Ring",
-		legs="Ebers Pantaloons +1",
-		feet="Piety duckbills +3"
+	sets.midcast.Cure = set_combine(sets.Idle,{
+		main		="Queller Rod",
+		ammo		="Hydrocera",
+		head		="Ebers cap +1",
+		neck		="Cleric's torque",
+		body		="Ebers bliaud +1",
+		left_ear	="Nourishing Earring +1",
+		right_ear	="Glorious Earring",
+		hands		="Bokwus Gloves",
+		back		="Alaunus's Cape",
+		waist		="Witful Belt",
+		left_ring	="Janniston Ring",
+		right_ring	="Tamas Ring",
+		legs		="Ebers Pantaloons +1",
+		feet		="Piety duckbills +3"
 	})	
 	
-	sets.WHM.Enhancing = set_combine(sets.WHM.Idle,{
-		main="Yagrush",
-		sub="Genbu's Shield",
-		head="Ebers cap +1",
-		neck="Colossus's Torque",
-		ear1="Magnetic Earring",
-		body="Cleric's Briault",
-		hands="Orison Mitts +2",
-		back="Merciful Cape",
-		waist="Embla Sash",
-		legs="Cleric's Pantaloons +2",
-		feet="Eber's Duckbills +1"
+	sets.midcast.Enhancing = set_combine(sets.Idle,{
+		main		="Gada",
+		sub			="Genbu's Shield",
+		head		="Telchine Cap",
+		neck		="Colossus's Torque",
+		left_ear	="Magnetic Earring",
+		body		="Telchine chasuble",
+		hands		="Telchine Gloves",
+		left_ring	="Stikini Ring +1",
+		right_ring	="Stikini Ring +1",
+		back		="Merciful Cape",
+		waist		="Embla Sash",
+		legs		="Telchine Braconi",
+		feet		="Theophany Duckbills +3"
 	})
 	
-	sets.WHM.Enfeebling = set_combine(sets.WHM.Idle,{
-		main="Queller Rod",
-		sub="Genbu's Shield",
-		ammo="Hedgehog Bomb",
-		head="Elite Beret",
-		body="Healer's Briault",
-		hands="Cleric's Mitts",
-		ring2="Kishar Ring",
-		ear1="Magnetic Earring",
-		back="Alaunus's Cape",
-		waist="Hierarch Belt",
-		legs="Augur's brais",
-		feet="Rubeus Boots",
-		neck="Spider Torque"
+	sets.midcast.Enfeebling = set_combine(sets.Idle,{
+		main		="Queller Rod",
+		sub			="Genbu's Shield",
+		ammo		="Hedgehog Bomb",
+		head		="Elite Beret",
+		body		="Healer's Briault",
+		hands		="Cleric's Mitts",
+		right_ring	="Kishar Ring",
+		left_ear	="Magnetic Earring",
+		back		="Alaunus's Cape",
+		waist		="Hierarch Belt",
+		legs		="Augur's brais",
+		feet		="Rubeus Boots",
+		neck		="Spider Torque"
 	})
 	
-	sets.WHM.BarSpells = set_combine(sets.WHM.Idle,{
-		main="Yagrush",
-		sub="Genbu's Shield",
-		ammo="Incantor Stone",
-		neck="Colossus's Torque",
-		ear1="Magnetic Earring",
-		body="Ebers Bliaud +1",
-		hands="Augur's gloves",
-		back="Merciful Cape",
-		legs="Cleric's Pantaloons +2",
-		feet="Orison Duckbills +2",
-		head="Orison Cap +2"
+	sets.midcast.BarElement = set_combine(sets.Idle,{
+		main		="Chatoyant Staff",
+		sub			="Genbu's Shield",
+		ammo		="Incantor Stone",
+		head		="Ebers Cap +1",
+		neck		="Colossus's Torque",
+		left_ear	="Magnetic Earring",
+		body		="Ebers Bliaud +1",
+		hands		="Ebers Mitts +1",
+		left_ring	="Stikini Ring +1",
+		right_ring	="Stikini Ring +1",
+		back		="Merciful Cape",
+		waist		="Embla Sash",
+		legs		="Piety Pantaloons +3",
+		feet		="Ebers Duckbills +1"
+	})
+
+	sets.midcast.BarStatus = set_combine(sets.Idle,{
+		main		="Chatoyant Staff",
+		sub			="Genbu's Shield",
+		ammo		="Incantor Stone",
+		head		="Ebers Cap +1",
+		neck		="Colossus's Torque",
+		left_ear	="Magnetic Earring",
+		body		="Ebers Bliaud +1",
+		hands		="Ebers Mitts +1",
+		left_ring	="Stikini Ring +1",
+		right_ring	="Stikini Ring +1",
+		back		="Merciful Cape",
+		waist		="Embla Sash",
+		legs		="Piety Pantaloons +3",
+		feet		="Ebers Duckbills +1"
 	})
 	
-	sets.WHM.Divine = set_combine(sets.WHM.Idle,{
-		main="Queller Rod",
-		sub="Genbu's Shield",
-		ammo="Incantor Stone",
-		neck="Inquisitor's Chain",
-		ear1="Novio Earring",
-		body="Auger's Jaseran",
-		hands="Cleric's mitts",
-		waist="Witch sash",
-		legs="Theophany Pantaloons",
-		feet="Rubeus Boots",
-		head="Marduk's Tiara"
+	sets.midcast.Divine = set_combine(sets.Idle,{
+		main		="Queller Rod",
+		sub			="Genbu's Shield",
+		ammo		="Incantor Stone",
+		neck		="Inquisitor's Chain",
+		left_ear	="Novio Earring",
+		body		="Auger's Jaseran",
+		hands		="Cleric's mitts",
+		waist		="Witch sash",
+		legs		="Theophany Pantaloons",
+		feet		="Rubeus Boots",
+		head		="Marduk's Tiara"
 	})
 	
 end
 
 function equipx(gearset)
 	if player.status == 'Engaged' then
-		equip(gearset,sets.WHM.Melee)
+		equip(gearset,sets.Engaged)
 	else
 		equip(gearset)
 	end
@@ -177,7 +198,7 @@ function precast(spell)
     if spell.type=="Item" then
         return
     end
-    if spell.type ~= 'WeaponSkill' and spell.type ~= 'JobAbility' then
+    if spell.action_type == 'Magic' then
         if buffactive.Silence then
             cancel_spell()
             if player.inventory['Echo Drops'] then
@@ -187,46 +208,50 @@ function precast(spell)
                 add_to_chat(123,'Silenced, you are out of Echo Drops!!!')	
             end
             return
-        end
-	end
-	if spell.type:endswith('Magic') then
-		equipx(sets.WHM.FastCast)
+		end
+		equipx(sets.precast.FastCast)
 	end
 end
 	
 function midcast(spell,action)	
 	if spell.type:endswith('Magic') then
 		if spell.skill == 'Healing Magic' then
-			if spell.english:startswith('Cure') or spell.english:startswith("Cura") then
-				equipx(sets.WHM.Cure)
-			elseif spell.english == 'Cursna' then
-				equipx(sets.WHM.Cursna)
+			if spell.english == 'Cursna' then
+				equipx(sets.midcast.Cursna)
+			elseif spell.english:startswith('Cur') then
+				equipx(sets.midcast.Cure)
 			else
-				equipx(sets.WHM.Healing)
+				equipx(sets.midcast.Healing)
 			end
 		elseif spell.skill == 'Enfeebling Magic' then
-			equipx(sets.WHM.Enfeebling)
+			equipx(sets.midcast.Enfeebling)
 		elseif spell.skill == 'Enhancing Magic' then
-			if spell.name=="Erase" then
-				equipx(set_combine(sets.WHM.Enhancing,{neck="Cleric's torque"}))
+			if spell.name == 'Erase' then
+				equipx(set_combine(sets.midcast.Enhancing,{neck="Cleric's torque"}))
+			elseif spell.name:startswith('Bar') then
+				if BarElement:contains(spell.name) then
+					equipx(sets.midcast.BarElement)
+				else
+					equipx(sets.midcast.Barstatus)
+				end
 			else
-				equipx(sets.WHM.Enhancing)
+				equipx(sets.midcast.Enhancing)
 			end
 		elseif spell.skill == 'Divine Magic' then
-			equipx(sets.WHM.Divine)
+			equipx(sets.midcast.Divine)
 		end
 	end
 end
 
 function aftercast(spell)
-	equipx(sets.WHM.Idle)
+	equipx(sets.Idle)
 end
 
 function status_change(new,old)
 	if new == 'Resting' then
-		equip(sets.WHM.Resting)
+		equip(sets.Resting)
 	else
-		equipx(sets.WHM.Idle)
+		equipx(sets.Idle)
 	end
 end
 
@@ -242,7 +267,7 @@ end
 
 function self_command(command)
 	if command == 'goIdle' then
-		equipx(sets.WHM.Idle)
+		equipx(sets.Idle)
 	end
 end
 
