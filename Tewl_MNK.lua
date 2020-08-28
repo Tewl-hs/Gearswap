704459137e40f1599322dc740b9efab1aff43c2b
Tew187 <60381764+Tew187@users.noreply.github.com>
2020-08-27 19:13:37 -0500

    -

--- Tewl_MNK.lua
+++ Tewl_MNK.lua
@@ -10,12 +10,48 @@
 --]]
     function get_sets()		      
         include('Modes.lua') -- Using Motes meta tables for modes
-        
+
         -- Personal settings: Load macros and set equipviewer position
         send_command('input /macro book 9;wait 0.2;input /macro set 1;wait 1;input /lockstyleset 4')
         send_command('input //equipviewer pos 1663 934')
 
         EngagedMode = M{['description'] = 'Engaged Mode', 'Normal', 'DT', 'Hybrid'}
+
+        --  NOT MY CODE! Testing content for displaying text under chat
+	    texts = require('texts')
+	    if stateBox then stateBox:destroy() end
+
+	    local settings = windower.get_windower_settings()
+	    local x,y
+
+	    if settings["ui_x_res"] == 1920 and settings["ui_y_res"] == 1080 then
+	    	x,y = settings["ui_x_res"]-1917, settings["ui_y_res"]-18 -- -285, -18
+	    else
+	    	x,y = 0, settings["ui_y_res"]-17 -- -285, -18
+	    end
+
+	    if displayx then x = displayx end
+	    if displayy then y = displayy end
+
+	    local font = displayfont or 'Arial'
+	    local size = displaysize or 12
+	    local bold = displaybold or true
+	    local bg = displaybg or 0
+	    local strokewidth = displaystroke or 2
+	    local stroketransparancy = displaytransparancy or 192
+
+	    stateBox = texts.new()
+	    stateBox:pos(x,y)
+	    stateBox:font(font)--Arial
+	    stateBox:size(size)
+	    stateBox:bold(bold)
+	    stateBox:bg_alpha(bg)--128
+	    stateBox:right_justified(false)
+	    stateBox:stroke_width(strokewidth)
+	    stateBox:stroke_transparency(stroketransparancy)
+
+	    update_status()
+        -- End of display code
 
         -- Variables for auto-skill chain. Only edit AutoWS 
         AutoWS = 'One Inch Punch'
@@ -307,3 +343,48 @@
             mov.counter = 0
         end
     end)
+
+    -- More code for displaying text -- Not finished 
+    function update_status()
+	    local clr = {
+            h='\\cs(255,192,0)', -- Yellow for active booleans and non-default modals
+		    w='\\cs(255,255,255)', -- White for labels and default modals
+            n='\\cs(192,192,192)', -- White for labels and default modals
+            s='\\cs(96,96,96)', -- Gray for inactive booleans
+		    Fire='\\cs(255,80,80)', -- Red For Fire Element
+		    Ice='\\cs(140,160,255)', -- Light Blue For Ice Element
+		    Wind='\\cs(110,255,110)', -- Light Green For Wind Element
+		    Earth='\\cs(220,214,110)', -- Brown/Yellow For Earth Element
+		    Lightning='\\cs(190,90,190)', -- Purple For Lightning Element
+		    Water='\\cs(110,110,255)', -- Blue For Water Element
+		    Light='\\cs(255,255,155)', -- Light Yellow For Light Element
+		    Dark='\\cs(90,90,90)', -- Dark Grey For Dark Element
+        }
+
+	    local spc = '    '
+
+        stateBox:clear()
+	    stateBox:append('   ')
+	    local status_text = ''
+        
+		status_text = string.format("%sEngaged: %s%s%s", clr.w, clr.h, EngagedMode.value, spc)
+	    stateBox:append(status_text)
+        stateBox:show()
+    end
+
+    function clear_job_states()
+        if stateBox then stateBox:destroy() end
+    end
+
+    windower.raw_register_event('outgoing chunk', function(id, data)
+        if id == 0x00D and stateBox then
+            stateBox:hide()
+        end
+    end)
+
+    windower.raw_register_event('incoming chunk', function(id, data)
+        if id == 0x00A and stateBox then
+            stateBox:show()
+        end
+    end)
+    -- End of Display Code
